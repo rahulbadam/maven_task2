d35070b6985d3d597eeb69557d68372f88f18594
AnishChatti <177r1a05k1@gmail.com>
2020-02-05 19:28:28 +0530

    First Commit

---
+++ maven-task/src/main/java/com/newyear/maven_task/mysweets/Chocolates.java
@@ -0,0 +1,13 @@
+package com.newyear.maven_task.mysweets;
+
+
+
+public class Chocolates extends sweets{
+
+	public void setdetails(String name,int weight)
+	{
+		sweet.put(name,weight);
+		}
+
+
+}
---
+++ maven-task/src/main/java/com/newyear/maven_task/mysweets/Cookies.java
@@ -0,0 +1,13 @@
+package com.newyear.maven_task.mysweets;
+
+
+
+public class Cookies extends sweets {
+	
+	public void setdetails(String name,int weight)
+	{
+		sweet.put(name,weight);
+	}
+
+
+}
